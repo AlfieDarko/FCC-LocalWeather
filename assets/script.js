590d3600b3958dcfb3122d80573de85063969b31
Alfie Darko <mrhazzy@gmail.com>
2017-02-14 21:20:01 +0000

    Main Functionality added
    Lost previous versions

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,26 @@
-$(document).ready(function () {
+ $(document).ready(function() {
+
     var city;
     var countryCode;
-    $.getJSON("http://ip-api.com/json", function (data2) {
-        city = data2.city;
-        countryCode = data2.countryCode;
-        console.log(city + countryCode)
-        var api = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + countryCode + '&appid=543038ac6ec11c62ded8555f02ceb1bc';
-        console.log(api)
-        
-        
-        $.getJSON(api, function (dataAPI) {
-            $("#weather-info").html( "city: " + city + "<br>country: " + countryCode + dataAPI);
-            
-            console.log(dataAPI)
+
+
+    $.getJSON( "http://ip-api.com/json", function( geoData ) {
+        city = geoData.city;
+        countryCode = geoData.countryCode;
+
+        url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ',' + countryCode + "&appid=543038ac6ec11c62ded8555f02ceb1bc&units=metric";
+
+        console.log(url);
+
+        $.getJSON( url, function (weatherData) {
+            $("#weatherContainer").html(weatherData.name + "<br>" + weatherData.weather[0].description
+            + "<br>" + Math.round(weatherData.main.temp) + "°C");
+            console.log(weatherData)
         })
-    });
+
+    })
+
+
+
+
 });
